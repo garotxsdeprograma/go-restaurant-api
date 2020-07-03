e112362bab57a8eaeadb5d3606c9fdad9ad4f9ad
Douglas Soares <sdouglas5107@gmail.com>
2020-07-02 21:59:20 -0300

    Products and Orders routes added

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const routes = require("./src/routes");
 const app = express();
 const port = process.env.PORT || 5555;
 
-app.get("", (req, res) => {
-  res.json({
-    message: "Deu bom truta",
-  });
-});
+app.use(routes);
 
-app.listen(port, (err) => { 
+app.listen(port, (err) => {
   if (err) return console.log(`Não startou ${err}`);
   console.log("Running on port", port);
 });
