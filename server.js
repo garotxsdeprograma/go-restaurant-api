a57b0e6944c778c5403fde107a7c707d5a279d9b
Douglas Soares <sdouglas5107@gmail.com>
2020-07-08 19:48:43 -0300

    Add cors and other bunch of stuff

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const routes = require("./src/routes");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 5555;
 
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(morgan("dev"));
 app.use(routes);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({ ...error, message: error.message });
+});
 
 app.listen(port, (err) => {
   if (err) return console.log(`Não startou ${err}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,10 @@ const router = new Router();
 router.use(orders);
 router.use(products);
 
+router.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
 module.exports = router;
