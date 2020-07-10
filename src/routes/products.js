c50faa8520184d844007120081bfb4df782a0fd4
Douglas Soares <sdouglas5107@gmail.com>
2020-07-09 22:04:08 -0300

    finnish all endpoints

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,37 +1,49 @@
 const { Router } = require("express");
 const router = new Router();
+const knex = require("../database");
 
 const routeName = "/products";
+const tableName = "products";
 
-//Lista todos os Produtos
 router.get(routeName, (req, res) => {
-  res.json([{ message: "Vai retornar todos os Produtos" }]);
+  knex(tableName).then((result) => res.json(result));
 });
 
-// Pega os dados de um Produto
 router.get(`${routeName}/:id`, (req, res) => {
-  res.json({
-    message: "Vai retornar os dados de um Produto dado um id",
-    id: req.params.id,
-  });
+  knex(tableName)
+    .where({ id: req.params.id })
+    .then(([found]) => res.json(found));
 });
 
-//Cria um Produto
 router.post(routeName, (req, res) => {
-  res.status(201).json({
-    message: "Vai criar um Produto",
-  });
+  knex(tableName)
+    .returning("*")
+    .insert(req.body)
+    .then(([inserted]) => res.status(201).json(inserted));
 });
 
-//Edita os dados de um Produto
-router.patch(`${routeName}/:id`, (req, res) => {
-  res.json({
-    message: "Vai editar os dados de um Produto dado um id",
-    id: req.params.id,
-  });
+router.patch(`${routeName}/:id`, async (req, res) => {
+  try {
+    const [found] = await knex(tableName).where({ id: req.params.id });
+    if (!found) {
+      const err = Error("Not Found");
+      err.status = 404;
+      throw err;
+    }
+    const updated = await knex(tableName)
+      .where({ id: req.params.id })
+      .update(req.body);
+    res.json(updated);
+  } catch (err) {
+    res.status(err.status || 500).json({ message: err.message });
+  }
 });
 
-//Deleta um Produto
-router.delete(`${routeName}/:id`, (req, res) => res.status(204).end());
+router.delete(`${routeName}/:id`, (req, res) => {
+  knex(tableName)
+    .where({ id: req.params.id })
+    .del()
+    .then(() => res.status(204).end());
+});
 
 module.exports = router;
